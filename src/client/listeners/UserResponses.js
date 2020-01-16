43e9ad757d851e75900e40c5ceb20a4aaaadf6d0
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-16 03:22:41 -0300

    new listener

---
+++ src/client/listeners/UserResponses.js
@@ -0,0 +1,51 @@
+const { Channel, Attachment } = require("discord.js");
+const { CanvasTemplates, Listener, Utils } = require("../../");
+
+module.exports = class UserResponses extends Listener {
+  constructor(client) {
+    super(client)
+
+    this.events = ['userLevelUp']
+    this.module = this.client.database && this.client.database.guilds;
+    this.social = client.database && client.database.users;
+  }
+
+  async onUserLevelUp(user, userDocument) {
+    const level = userDocument.level;
+    const channel = user.channel;
+
+    try {
+      const insertNewLevel = await this.social.update(user.id, {
+        $push: {
+          'economy.levels': {
+            level, maxXp: Utils.XPtoNextLevel(userDocument.level)
+          }
+        }
+      })
+
+      // Send
+
+      if (channel instanceof Channel) {
+        const { systemsDisabled } = await this.module.findOne(channel.guild.id, 'systemsDisabled');
+
+        if (systemsDisabled && systemsDisabled.some(system => system.name.toUpperCase() === 'LEVEL_UP')) return;
+
+        const t = this.client.language.lang(user.language);
+        const updateImage = await CanvasTemplates.levelUpdated(user.user, t, userDocument)
+
+        channel
+          .send(
+            t('commons:economy.levelup', { level, username: user.username }),
+            new Attachment(updateImage, `levelup-${user.id}.jpg`)
+          )
+          .catch(() => null)
+
+        return insertNewLevel
+      }
+    } catch (e) {
+      this.client.console(true, (e.stack || e), this.constructor.name);
+    }
+
+    return true
+  }
+}
