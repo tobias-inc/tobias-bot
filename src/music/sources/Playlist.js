06b8a8d7780b78478620291582c295be64880c9a
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-18 19:36:00 -0300

    now says the loading time

--- src/music/sources/Playlist.js
+++ src/music/sources/Playlist.js
@@ -1,7 +1,9 @@
+const moment = require("moment");
 const PlaylistStructure = require("../structures/Playlist.js");
 
 module.exports = class Playlist extends PlaylistStructure {
-  constructor(songs, playlistInfo, requestedBy) {
+  constructor(songs, playlistInfo, requestedBy, { startedAt, finishedAt }) {
     super(songs, playlistInfo, requestedBy)
+    this.loadTime = moment.duration(finishedAt - startedAt).format('m[m] s[s]', { stopTrim: 's' });
   }
 }
