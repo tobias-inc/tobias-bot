4abfaf4ab5632413ec8ef7a729ab6f5796eb89b7
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-18 20:15:16 -0300

    'trim' and 'precision' propertys from moment duration method added

--- src/music/sources/Playlist.js
+++ src/music/sources/Playlist.js
@@ -4,6 +4,9 @@ const PlaylistStructure = require("../structures/Playlist.js");
 module.exports = class Playlist extends PlaylistStructure {
   constructor(songs, playlistInfo, requestedBy, { startedAt, finishedAt }) {
     super(songs, playlistInfo, requestedBy)
-    this.loadTime = moment.duration(finishedAt - startedAt).format('m[m] s[s]', { stopTrim: 's' });
+    this.loadTime = moment.duration(finishedAt - startedAt).format('m [minutes] s [seconds]', {
+      trim: true,
+      precision: 3
+    })
   }
 }
