300c6e22e3a307c689f07e05a65e103126c2f709
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-18 19:36:37 -0300

    guild queue class received new methods

--- src/music/structures/Queue.js
+++ src/music/structures/Queue.js
@@ -7,12 +7,16 @@ module.exports = class GuildQueue extends Array {
     return this.length;
   }
 
-  push(song) {
-    return super.push(song);
-  }
-
   purge() {
     return this.splice(0)
+  }
+
+  shuffle() {
+    return this.sort(() => Math.random() > 0.5 ? -1 : 1)
+  }
+
+  remove(index) {
+    return this.queue.splice(index, 1)[0]
   }
 
   removeSong(num) {
