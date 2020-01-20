6ff2f56d43b14d8c627e5b4b44e54091be5a19cc
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-19 22:06:22 -0300

    deleted

--- src/music/sources/Song.js
+++
@@ -1,7 +0,0 @@
-const SongStructure = require("../structures/Song.js");
-
-module.exports = class Song extends SongStructure {
-  constructor(song, requestedBy) {
-    super(song, requestedBy)
-  }
-}
