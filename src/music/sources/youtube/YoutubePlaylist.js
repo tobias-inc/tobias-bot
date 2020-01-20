589859490ea3a45f605ad3bbae91f480acb38de4
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-19 22:45:16 -0300

    locales

--- src/music/sources/youtube/YoutubePlaylist.js
+++ src/music/sources/youtube/YoutubePlaylist.js
@@ -1,13 +1,12 @@
 const { Playlist } = require("../../structures");
-const YoutubeSong = require("./YoutubeSong.js");
 
 const PLAYLIST_URI = 'https://www.youtube.com/playlist?list=';
 
 module.exports = class YoutubePlaylist extends Playlist {
-  constructor(data = {}, songs = [], requestedBy, Youtube) {
+  constructor(data = {}, songs = [], requestedBy) {
     super(data, songs, requestedBy)
 
     this.uri = PLAYLIST_URI + this.identifier
-    this._Youtube = Youtube
+    this.title = this.name
   }
 }
