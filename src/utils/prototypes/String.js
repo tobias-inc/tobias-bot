0d3f8c7195bc715a927300403a214a81253e1ad0
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-21 21:10:52 -0300

    more prototypes added

--- src/utils/prototypes/String.js
+++ src/utils/prototypes/String.js
@@ -5,12 +5,32 @@ module.exports = class StringPrototypes extends Prototype {
     // capitalize
 
     /**
-     * @returns {String}
+     * @returns {string}
      */
     String.prototype.capitalize = function capitalize() {
       return this.charAt(0).toUpperCase() + this.slice(1)
     }
 
     //
+
+    // allReplaces
+
+    /**
+     * @param {object} replaces 
+     * @returns {string} String replaced by entered values
+     */
+    String.prototype.allReplaces = function allReplaces(replaces) {
+      let str = this
+
+      if (!(replaces instanceof Object)) return this
+
+      Object.entries(replaces).forEach(([k, v]) => {
+        const replaceRegex = new RegExp(k, 'gi');
+        str = str.replace(replaceRegex, v)
+        return str
+      })
+
+      return str
+    }
   }
 }
