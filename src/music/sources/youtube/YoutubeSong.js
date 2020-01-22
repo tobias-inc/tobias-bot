6e6b6439dc3379afb4e6b1fb49b68ee7fb404fd0
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-21 21:08:35 -0300

    ...

--- src/music/sources/youtube/YoutubeSong.js
+++ src/music/sources/youtube/YoutubeSong.js
@@ -1,9 +1,19 @@
+const fetch = require("node-fetch");
+
 const { Song } = require("../../structures");
 const Constants = require("../../../utils/Constants.js");
 
 module.exports = class YoutubeSong extends Song {
   constructor(data = {}, requestedBy) {
     super(data, requestedBy)
+    this.artwork = `https://i.ytimg.com/vi/${this.identifier}/hqdefault.jpg`
     this.color = Constants.YOUTUBE_COLOR
+    this.source = 'youtube'
+  }
+
+  getdArtwork() {
+    return fetch(`https://i.ytimg.com/vi/${this.identifier}/maxresdefault.jpg`)
+      .then(res => res.status !== 404)
+      .catch(() => false)
   }
 }
