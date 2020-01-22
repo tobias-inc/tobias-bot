92c8a292ad25e18f590e82d6d8926ba95950f62c
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-22 14:40:45 -0300

    new func added --> replaceTime

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,30 @@
 module.exports = {
-  XPtoNextLevel: (level = 1) => 315 * level
-}
+  XPtoNextLevel: (level = 1) => 215 * (level + 1),
+  replaceTime: (i18, time) => {
+    const t = typeof i18 === 'function' ? i18 : null
+    if (!t) throw new TypeError('CONTEXT NOT SET')
+    if (typeof time !== 'string') return time
 
-// XPtoNextLevel: (level) => {
-//   const perLevel = level + 1;
-//   return (((level * 135) * level) + (30 * level)) * perLevel;
-// }
+    const timeResponses = t('commons:timeResponses', { returnObjects: true });
+    const reviews = ['s', 'm', 'h', 'd', 'w', 'mo', 'y']
+
+    let timeStr = time
+      .split(' ')
+      .map(str => {
+        const timeR = parseInt(str.replace(/[^0-9]+/, ''));
+        const keyR = str.replace(/[^a-zA-Z]+/, '');
+
+        let keyLower = keyR.toLowerCase()
+        if (reviews.includes(keyLower)) {
+          return str.replace(new RegExp(keyR), () => {
+            const timeResponse = timeResponses.find(key => key[keyR])
+            const value = timeR > 1 ? timeResponse[`${keyR}_plural`] : timeResponse[keyR]
+            return ` ${value}`
+          })
+        }
+        return str
+      })
+
+    return timeStr.join(' ')
+  }
+}
