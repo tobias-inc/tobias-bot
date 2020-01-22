fa6e37e0da9ecd966e41fb536b0f71ea8b24935e
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-21 21:03:30 -0300

    rework

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 if (process.env.NODE_ENV !== 'development') require('dotenv').config()
 
-const clientConfig = {
+require("moment");
+require("moment-duration-format");
+
+let canvasLoaded = false
+try {
+  require("canvas")
+  require("./src/utils/canvas/CanvasUtils.js").initializeHelpers()
+  canvasLoaded = true
+} catch (e) {
+  console.log(e)
+}
+
+const CLIENT_OPTIONS = {
   disabledEvents: ["TYPING_START", "TYPING_STOP", "USER_NOTE_UPDATE"],
   disableEveryone: true,
+  fetchAllMembers: true,
   restTimeOffset: 2000,
   retryLimit: 20,
   messageCacheMaxSize: 2024,
   messageCacheLifetime: 1680,
   messageSweepInterval: 1680,
   autoReconnect: true,
-  canvasLoaded: true
+  canvasLoaded
 }
 
-try {
-  require("./src/utils/canvas/CanvasUtils.js").initializeHelpers()
-} catch (e) {
-  console.log(e)
-  clientConfig.canvasLoaded = false
-}
-
-const client = require('./src/TobiasClient.js');
-
-module.exports = new client(clientConfig).login()
+const tobias = require('./src/TobiasClient.js');
+const client = new tobias(CLIENT_OPTIONS);
+client
+  .login()
+  .then(() => client.console(false, 'I successfully connected!', 'LOGIN', 'DISCORD API'))
