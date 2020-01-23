5b1768785db87f814bc513c379c75d293f5a6806
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-23 18:44:05 -0300

    command parameter added

--- src/commands/developer/reload.js
+++ src/commands/developer/reload.js
@@ -5,25 +5,16 @@ module.exports = class Reload extends Command {
     super(client, path, {
       name: 'reload',
       category: 'developer',
-      aliases: ['r'],
       hidden: true,
       utils: {
         requirements: { devOnly: true },
-        parameters: [{
-          type: 'string',
-          full: true,
-          whitelist: (arg) => this.client.commands.find(({ name, aliases }) => (name === arg.toLowerCase()) || aliases.includes(arg.toLowerCase())),
-          missingError: ({ t, author }) => {
-            return new ClientEmbed(author).setTitle(t('commands:reload.noCommand'))
-          }
-        }]
+        parameters: [{ type: 'command', missingError: 'commands:reload.noCommand' }]
       }
     })
   }
 
-  async run({ channel, author, t }, cmd) {
+  async run({ channel, author, t }, command) {
     try {
-      const command = this.client.commands.find(({ name, aliases }) => (name === cmd.toLowerCase()) || aliases.includes(cmd.toLowerCase()));
       await command.reload()
       channel.send(new ClientEmbed(author).setTitle(t('commands:reload.sucess', { command: command.name })))
     } catch (e) {
---
+++ src/structures/command/parameters/types/CommandParameter.js
@@ -0,0 +1,38 @@
+const UserParameter = require("./UserParameter.js");
+
+const regexpSpecialChars = /([[\]^$|()\\+*?{}=!.])/gi
+const quoteRegex = (text) => text.replace(regexpSpecialChars, '\\$1')
+const prefixRegex = (prefix) => new RegExp(`^${quoteRegex(prefix)}`)
+
+module.exports = class CommandParameter extends UserParameter {
+  static parseOptions(options = {}) {
+    return {
+      ...super.parseOptions(options),
+      full: true,
+      validCommands: !!options.validCommands,
+      getSubcommands: !!options.getSubcommands
+    }
+  }
+
+  static parse(arg, { client, prefix }) {
+    if (!arg) return
+
+    arg = arg.replace(prefixRegex(prefix), '')
+    const [cmd, subcmd] = arg.split(' ')
+
+    const command = client.commands
+      .filter(c => this.validCommands && !c.hidden)
+      .find(c => (c.name === cmd.toLowerCase()) || c.aliases.includes(cmd.toLowerCase()))
+
+    if (command) {
+      if (this.getSubcommands && subcmd) {
+        const subcommand = command.subcommands.find(
+          s => (s.name === subcmd.toLowerCase()) || s.aliases.includes(subcmd.toLowerCase())
+        )
+        if (subcommand) return subcommand
+      }
+
+      return command
+    }
+  }
+}
