445976241823a602f759bc84c50bf6e50080da67
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-27 22:31:31 -0300

    hastebin api

---
+++ src/apis/Hastebin.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+const { Wrapper } = require("../");
+
+const API_URL = 'https://hastebin.com';
+const RAW_URL = 'https://hastebin.com/raw';
+
+module.exports = class HastebinWrapper extends Wrapper {
+  constructor() {
+    super('hastebin')
+  }
+
+  createPaste(text) {
+    return this.request('/documents', 'POST', text).then(({ key }) => {
+      if (key) return `${RAW_URL}/${key}`
+    })
+  }
+
+  request(endpoint, method = 'GET', body = {}) {
+    return fetch(`${API_URL}${endpoint}`, { method, body }).then(res => res.json())
+  }
+}
