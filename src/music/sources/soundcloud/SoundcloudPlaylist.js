3a42966bd877fbaeb2e033f3f4a8fb86f90cd368
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-28 16:29:29 -0300

    SoundcloudPlaylist

---
+++ src/music/sources/soundcloud/SoundcloudPlaylist.js
@@ -0,0 +1,14 @@
+const { Playlist } = require("../../structures");
+const SoundcloudSong = require("./SoundcloudSong.js");
+
+module.exports = class SoundcloudPlaylist extends Playlist {
+  constructor(data = {}, songs = [], requestedBy) {
+    super(data, songs, requestedBy)
+    this.source = 'soundcloud'
+  }
+
+  loadInfo() {
+    this.songs = this.songs.map(s => new SoundcloudSong(s, this.requestedBy))
+    return this
+  }
+}
