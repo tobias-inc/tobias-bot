da3306ef3cd6a242a99328091c16ebe2ba0ef7ee
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-29 14:49:10 -0300

    rework

--- src/music/structures/SongSearchResult.js
+++ src/music/structures/SongSearchResult.js
@@ -1,15 +1,20 @@
 module.exports = class SongSearchResult {
-  constructor(tryAgain = true) {
+  constructor(tryAgain = true, loadTime) {
     this.tryAgain = tryAgain
+    this.loadTime = loadTime
   }
 
   async setResult(result) {
+    if (result) result.loadTime = this.loadTime
     this.result = await result
     return this
   }
 
   static async from(result, tryAgain = true) {
-    const searchResult = new this(tryAgain)
-    return searchResult.setResult(await result)
+    const now = Date.now()
+    result = await result
+
+    const searchResult = new this(tryAgain, Date.now() - now)
+    return searchResult.setResult(result)
   }
 }
