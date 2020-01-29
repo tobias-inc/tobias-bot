7aeeb851dd57c8ea644ad52060435bbae1d953c6
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-01-29 14:48:53 -0300

    fix music sources and optmize

--- src/music/sources/spotify/SpotifySongSource.js
+++ src/music/sources/spotify/SpotifySongSource.js
@@ -4,12 +4,9 @@ const SpotifySong = require("./SpotifySong.js");
 
 module.exports = class SpotifySongSource extends SongSource {
   static get customSources() {
-    const albumHandler = ([, id], m, r) => this.provideAlbum(m, id, r)
-    const playlistHandler = ([, id], m, r) => this.providePlaylist(m, id, r)
-    const trackHandler = async ([, id], m, r) => {
-      const track = await m.client.apis.spotify.getTrack(id)
-      return this.provideTrack(m, track, r)
-    }
+    const albumHandler = ([, id], m, r) => this.provideAlbum(m, id, r);
+    const playlistHandler = ([, id], m, r) => this.providePlaylist(m, id, r);
+    const trackHandler = ([, id], m, r) => this.getTrack(m, id, r);
 
     return [
       [
@@ -27,29 +24,34 @@ module.exports = class SpotifySongSource extends SongSource {
     ]
   }
 
+  static async getTrack(manager, id, requestedBy) {
+    const track = await manager.client.apis.spotify.getTrack(id)
+    return this.provideTrack(manager, track, requestedBy)
+  }
+
   static async providePlaylist(manager, id, requestedBy) {
     const playlist = await manager.client.apis.spotify.getPlaylist(id)
     if (!playlist) return
 
-    const { items } = await manager.client.apis.spotify.getPlaylistTracks(playlist.id)
+    const { items } = playlist.tracks
     const videos = (await Promise.all(items.map(({ track }) => this.provideTrack(manager, track, requestedBy)))).filter(i => !!i)
-    return new SpotifyPlaylist(playlist, videos, requestedBy).loadInfo()
+    return new SpotifyPlaylist(playlist, videos, requestedBy)
   }
 
   static async provideAlbum(manager, id, requestedBy) {
     const album = await manager.client.apis.spotify.getAlbum(id)
     if (!album) return
 
-    const { items } = await manager.client.apis.spotify.getAlbumTracks(album.id)
-    const videos = (await Promise.all(items.map((track) => this.provideTrack(manager, track, requestedBy, album)))).filter(i => !!i)
-    return new SpotifyPlaylist(album, videos, requestedBy).loadInfo()
+    const { items } = album.tracks
+    const videos = (await Promise.all(items.map(track => this.provideTrack(manager, track, requestedBy, album)))).filter(i => !!i)
+    return new SpotifyPlaylist(album, videos, requestedBy)
   }
 
   static async provideTrack(manager, track, requestedBy, album = track.album) {
     try {
       const song = await this.getClosestVideo(manager, track);
       if (song) {
-        return new SpotifySong(song, requestedBy, track, album).loadInfo()
+        return new SpotifySong(song, requestedBy, track, album)
       }
     } catch (e) {
       manager.client.console(true, e)
