35ff0e229f3abe9ab0ee8ae6c9b41f697e7d21c0
Pai do deuso Tobias <48861116+Asynchronous301@users.noreply.github.com>
2020-02-23 20:42:07 -0300

    Command add

--- src/commands/utility/addemojis.js
+++ src/commands/utility/addemojis.js
@@ -1,8 +1,12 @@
-const { Command } = require('../..')
-// const { Attachment } = require('discord.js')
+const {
+  Discord = require("discord.js"),
+  Attachment = Discord.Attachment,
+  Command, ClientEmbed, Constants
+} = require('../..')
+
 
 module.exports = class AddEmoji extends Command {
-  constructor (client, path) {
+  constructor(client, path) {
     super(client, path, {
       name: 'addemoji',
       category: 'utility',
@@ -10,5 +14,29 @@ module.exports = class AddEmoji extends Command {
     })
   }
 
-  async run ({ channel, guild, message, args }, t) {}
+  async run({ channel, guild, args }, t) {
+
+    const embed = new ClientEmbed(author, { author: [this.client.user] })
+
+    if (!args) {
+      return embed
+        .setDescription(Emojis.Errado + "**" + author.username + "**" + t('comandos:emojiinfo.noArgs'))
+        .setColor(Constants.ERROR_COLOR)
+
+    }
+
+    let emojo = false
+
+    emojo = await this.GetEmoji(args[0], guild);
+
+    if (!emojo) {
+      return embed
+        .setDescription(Emojis.Errado + "**" + author.username + "**" + t('comandos:emojiinfo.noEmoji', { searsh: args[0] }))
+        .setColor(Constants.ERROR_COLOR)
+
+    }
+    let type = emojo.animated ? '.gif' : '.png'
+    let emoji = new Attachment(emojo.url, emojo.name + type);
+    channel.send(`\`${emojo.name}\``, emoji || embed)
+  }
 }
