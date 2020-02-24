a35ad67be3ac97db8a7d36e349847a01357424c7
AvengerSuicide <dev.avenger.suicide@gmail.com>
2020-02-24 02:14:24 -0300

    fixes

--- src/commands/economy/money.js
+++ src/commands/economy/money.js
@@ -1,28 +1,31 @@
 const { Command, ClientEmbed } = require('../../')
 
 module.exports = class Money extends Command {
-    constructor (client, path) {
-        super(client, path, {
-            name: 'money',
-            category: 'economy',
-            utils: {
-                requirements: { databaseOnly: true }
-            },
-            parameters: [{
-                type: 'user', full: true, required: false
-            }]
-        })
-    }
+  constructor (client, path) {
+    super(client, path, {
+      name: 'money',
+      category: 'economy',
+      utils: {
+        requirements: { databaseOnly: true },
+        parameters: [
+          {
+            type: 'user',
+            full: true,
+            required: false
+          }
+        ]
+      }
+    })
+  }
 
-    async run ({ t, author, channel }, user) {
-        const embed = new ClientEmbed(author)
+  async run ({ t, author, channel }, user = author) {
+    const embed = new ClientEmbed(author)
+    const balance = await this.client.controllers.economy.balance(user.id)
 
-        const balance = await this.client.controllers.economy.balance(user.id)
-        if (author.id === user.id) embed.setDescription(t('commands:money.authorMoney', { balance }))
-        else { embed.setDescription(t('commands:money.userMoney', { user: user.tag, balance })) }
-
-        return channel.send(
-            embed
-        )
-    }
+    channel.send(
+      embed.setDescription(
+        t('commands:money.userMoney', { user: user.tag, balance })
+      )
+    )
+  }
 }
