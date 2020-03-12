d99a8cf7e591a821ee34f995801d3cdfa8f7b161
AvengerSuicide <dev.avenger.suicide@gmail.com>
2020-03-12 19:51:00 -0300

    lyrics api - Vagalume

---
+++ src/apis/Vagalume.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch')
+const { Wrapper } = require('../')
+
+const API_URL = 'https://api.vagalume.com.br'
+
+const generateArtistSerch = artist =>
+  `https://www.vagalume.com.br/${artist}/index.js`
+
+module.exports = class GeniusWrapper extends Wrapper {
+  constructor () {
+    super('vagalume')
+    // this.envVars = ['VAGALUME_KEY']
+  }
+
+  getLyric (songName) {
+    return request('/search.artmus')
+  }
+
+  getArtist (artist) {
+    return fetch(generateArtistSerch(artist)).then(res => res.json())
+  }
+
+  request (endpoint, queryParams = {}) {
+    const qParams = new URLSearchParams(queryParams)
+    return fetch(`${API_URL}${endpoint}?${qParams.toString()}`).then(res => res.json())
+  }
+}
