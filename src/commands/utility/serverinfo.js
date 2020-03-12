85e61712efb39fd728d445a8328190fbf7bde3d8
AvengerSuicide <dev.avenger.suicide@gmail.com>
2020-03-12 19:51:33 -0300

    not worked

--- src/commands/utility/serverinfo.js
+++ src/commands/utility/serverinfo.js
@@ -10,58 +10,97 @@ module.exports = class ServerInfo extends Command {
     })
   }
 
-  async run ({ channel, language, t, author, message, guild }, user = author) {
+  async run ({ channel, language, t, author, guild }) {
     const embed = new ClientEmbed(author)
 
     channel.send(
-
       embed
-
         .addField(t('commands:serverinfo.name'), guild.name, true)
-        .addField(t('commands:serverinfo.verify.ctx'), t(`commands:serverinfo.verify.level.${guild.verificationLevel}`), true)
-        .addField(t('commands:serverinfo.createdAt'), (await this.Time(guild.createdAt, language)), false)
+        .addField(
+          t('commands:serverinfo.verify.ctx'),
+          t(`commands:serverinfo.verify.level.${guild.verificationLevel}`),
+          true
+        )
+        .addField(
+          t('commands:serverinfo.createdAt'),
+          await this.time(guild.createdAt, language),
+          false
+        )
         .addField(t('commands:serverinfo.owner'), guild.owner.user.tag, true)
-        // .addField(t('commands:serverinfo.region'), this.client.regionsLang.replaces[guild.region], true)
-        .addField(t('commands:serverinfo.channels.ctx', { length: Number(guild.channels.size).localeNumber(language) }), (await this.Channels(guild, t, language)), true)
-        .addField(t('commands:serverinfo.members.ctx', { size: Number(guild.memberCount).localeNumber(language) }), (await this.Members(guild, t, language)), true)
-        .addField(t('commands:serverinfo.role.ctx', { length: Number(guild.roles.size - 1).localeNumber(language) }), (await this.Roles(guild, t, language)), false)
+        .addField(
+          t('commands:serverinfo.channels.ctx', {
+            length: Number(guild.channels.size).localeNumber(language)
+          }),
+          await this.channels(guild, t, language),
+          true
+        )
+        .addField(
+          t('commands:serverinfo.members.ctx', {
+            size: Number(guild.memberCount).localeNumber(language)
+          }),
+          await this.members(guild, t, language),
+          true
+        )
+        .addField(
+          t('commands:serverinfo.role.ctx', {
+            length: Number(guild.roles.size - 1).localeNumber(language)
+          }),
+          await this.roles(guild, t, language),
+          false
+        )
     )
   }
 
-  Channels (guild, t, lang) {
-    const CATEGORY = (`**${Number(guild.channels.filter(c => c.type === 'category').size).localeNumber(lang)}**`)
-    const VOICE = (`**${Number(guild.channels.filter(c => c.type === 'voice').size).localeNumber(lang)}**`)
-    const TEXT = (`**${Number(guild.channels.filter(c => c.type === 'text').size).localeNumber(lang)}**`)
+  channels (guild, t, lang) {
+    const category = `**${Number(
+      guild.channels.filter(c => c.type === 'category').size
+    ).localeNumber(lang)}**`
+    const voice = `**${Number(
+      guild.channels.filter(c => c.type === 'voice').size
+    ).localeNumber(lang)}**`
+    const text = `**${Number(
+      guild.channels.filter(c => c.type === 'text').size
+    ).localeNumber(lang)}**`
     return [
-      t('commands:serverinfo.channels.category') + CATEGORY,
-      t('commands:serverinfo.channels.text') + TEXT,
-      t('commands:serverinfo.channels.voice') + VOICE
+      t('commands:serverinfo.channels.category') + category,
+      t('commands:serverinfo.channels.text') + text,
+      t('commands:serverinfo.channels.voice') + voice
     ].join('\n')
   }
 
-  Members (guild, t, lang) {
-    const USERS = (`**${Number(guild.memberCount - guild.members.filter(u => !u.user.bot).size).localeNumber(lang)}**`)
-    const BOTS = (`**${Number(guild.members.filter(u => u.user.bot).size).localeNumber(lang)}**`)
+  members (guild, t, lang) {
+    const users = `**${Number(
+      guild.memberCount - guild.members.filter(u => !u.user.bot).size
+    ).localeNumber(lang)}**`
+    const bots = `**${Number(
+      guild.members.filter(u => u.user.bot).size
+    ).localeNumber(lang)}**`
     return [
-      t('commands:serverinfo.members.users') + USERS,
-      t('commands:serverinfo.members.bots') + BOTS
+      t('commands:serverinfo.members.users') + users,
+      t('commands:serverinfo.members.bots') + bots
     ].join('\n')
   }
 
-  Roles (guild, t, lang) {
-    const ROLES = guild.roles.map(role => role).slice(1)
-    const MANAGED = guild.roles.filter(role => role.managed)
-    if (!ROLES.length) return t('commands:serverinfo.role.noTags')
+  roles (guild, t, lang) {
+    const roles = guild.roles.map(role => role).slice(1)
+    const managed = guild.roles.filter(role => role.managed)
+    if (!roles.length) return t('commands:serverinfo.role.noTags')
     return [
-      t('commands:serverinfo.role.managed') + `**${Number(MANAGED.size).localeNumber(lang)}**`,
-      (ROLES.length > 10
-        ? ROLES.map(r => r).slice(0, 10).join(', ') + ` ${t('commands:serverinfo.role.tags', { length: (ROLES.length - 10) })}`
-        : ROLES.map(r => r).join(', ')
-      )
+      t('commands:serverinfo.role.managed') +
+        `**${Number(managed.size).localeNumber(lang)}**`,
+      roles.length > 10
+        ? roles
+          .map(r => r)
+          .slice(0, 10)
+          .join(', ') +
+          ` ${t('commands:serverinfo.role.tags', {
+            length: roles.length - 10
+          })}`
+        : roles.map(r => r).join(', ')
     ].join('\n')
   }
 
-  Time (ms, language) {
+  time (ms, language) {
     moment.locale(language)
     return moment(ms).format('LLLL')
   }
